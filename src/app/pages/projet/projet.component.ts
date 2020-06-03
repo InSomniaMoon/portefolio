2de14b5d6b1c820c495cd006e55c00306be61138
Pierre Leroyer <pierre.leroyer69@gmail.com>
2020-06-03 17:28:41 +0200

    add projet page

--- src/app/pages/projet/projet.component.ts
+++ src/app/pages/projet/projet.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProjectService } from 'src/app/core/services/project.service';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Projet } from 'src/app/core/models/Projet';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-projet',
@@ -10,12 +11,16 @@ import { Projet } from 'src/app/core/models/Projet';
 })
 export class ProjetComponent implements OnInit {
   public projet: Projet;
-  constructor(private $project: ProjectService, private route: ActivatedRoute) { }
+  constructor(private $project: ProjectService, private route: ActivatedRoute, private location:Location) { }
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
-      this.$project.getProjectBySlug(params.slug).subscribe((data => this.projet = data));
+      this.$project.getProjectBySlug(params.slug).subscribe((data => {this.projet = data; console.log(data)}));
     });
+  }
+
+  goBack() {
+    this.location.back();
   }
 
 }
